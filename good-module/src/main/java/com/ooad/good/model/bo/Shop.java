67da2de7abcaf7c7e818a8f7de7df6befc0c3a95
ESZAN <619589297@qq.com>
2020-12-14 20:26:27 +0800

    --story=1001467 --user=EZAN 商品模块之店铺服务 https://www.tapd.cn/46092103/s/1001030

---
+++ good-module/src/main/java/com/ooad/good/model/bo/Shop.java
@@ -0,0 +1,75 @@
+package com.ooad.good.model.bo;
+
+import cn.edu.xmu.ooad.model.VoObject;
+import com.ooad.good.model.po.ShopPo;
+import com.ooad.good.model.vo.ShopRetVo;
+import com.ooad.good.model.vo.ShopSimpleRetVo;
+import com.ooad.good.model.vo.ShopVo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+
+@Data
+public class Shop implements VoObject {
+	private Long id;
+	private String name;
+	private Byte state;
+	private LocalDateTime gmtCreate;
+	private LocalDateTime gmtModified;
+
+	public Shop(){};
+	public Shop(Long shopId) {
+		this.id = shopId;
+	}
+
+	public ShopPo createUpdatePo(ShopVo vo){
+		ShopPo po=new ShopPo();
+		po.setId(this.getId());
+		po.setGmtCreate(this.getGmtCreate());
+		po.setGmtModified(LocalDateTime.now());
+		po.setState(this.getState());
+		po.setName(vo.getName());
+		return po;
+
+	}
+
+	/**
+	 * 用po构造bo对象
+	 * @param po
+	 */
+	public Shop(ShopPo po){
+		this.id=po.getId();
+		this.name=po.getName();
+		this.state=po.getState();
+		this.gmtCreate=po.getGmtCreate();
+		this.gmtModified=po.getGmtModified();
+
+	}
+
+
+	/**
+	 * 用bo对象创建更新po对象
+	 * @return
+	 */
+	public ShopPo gotShopPo(){
+		ShopPo po=new ShopPo();
+		po.setId(this.getId());
+		po.setName(this.getName());
+		po.setState(this.getState());
+		po.setGmtCreate(this.gmtCreate);
+		po.setGmtModified(this.gmtModified);
+		return po;
+	}
+
+	@Override
+	public Object createVo() {
+		return new ShopRetVo(this);
+	}
+
+	@Override
+	public Object createSimpleVo() {
+		return new ShopSimpleRetVo(this);
+	}
+}
